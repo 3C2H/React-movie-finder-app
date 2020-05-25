db0d6180c98aefc9c841ebad762da934625afa43
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-05-25 17:31:16 +0200

    add movie search logic

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-export default function SearchMovies() {
+function SearchMovies() {
+
+  const handleSearch = async(e) => {
+    e.preventDefault()
+    console.log('sending')
+
+    const apiKey = "5dcf7f28a88be0edc01bbbde06f024ab"// your API key
+    const query = "Tron" 
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
+
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      console.log(data)
+    }catch(err) {
+      console.error(err)
+    }
+
+  }
+
   return(
-    <form className= 'form'>
+    <form className= 'form' onSubmit={handleSearch}>
       <label className='label' htmlFor='query'>Movie Name</label>
       <input className='input' type='text' name='query' placeholder='exemple: Tron'></input>
       <button className='button' type='submit'>Search</button>
     </form>
   )
 }
+
+export default SearchMovies
