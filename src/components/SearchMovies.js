ad59d4d4ef4da3f59dfc06b0ef3e2d8abd4857fa
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-05-27 13:55:18 +0200

    add movie cards style

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -40,7 +40,7 @@ function SearchMovies() {
         </input>
         <button className='button' type='submit'>Search</button>
       </form>
-      <div className='cards-list'>
+      <div className='card-list'>
         {movies.filter(movie => movie.poster_path).map(movie => (
           <div className='card' key={movie.id}>
             <img className='card--image'
